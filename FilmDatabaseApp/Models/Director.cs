b27e74384f08d6688420f53a5e043cb588bcdc59
Weiskri <kkweis13@gmail.com>
2018-03-07 10:44:33 -0500

    initial commit

---
+++ FilmDatabaseApp/Models/Director.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FilmDatabaseApp.Models
+{
+    public class Director
+    {
+        [Key]
+        public int DirectorID { get; set; }
+        public string Name { get; set; }
+
+        // navigation properties
+        public virtual ICollection<Movie> Movies { get; set; }
+
+    }
+}
---
+++ FilmDatabaseApp/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FilmDatabaseApp.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int GenreID { get; set; }
+        public string Type { get; set; }
+
+        // navigation properties
+        public virtual ICollection<Movie> Movies { get; set; }
+
+    }
+}
