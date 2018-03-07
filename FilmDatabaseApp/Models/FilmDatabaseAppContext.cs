09e5c95d2663ef0a169137721b6b62fc6b602eec
Weiskri <kkweis13@gmail.com>
2018-03-07 11:14:37 -0500

    added controllers

---
+++ FilmDatabaseApp/Models/FilmDatabaseAppContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FilmDatabaseApp.Models
+{
+    public class FilmDatabaseAppContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public FilmDatabaseAppContext() : base("name=FilmDatabaseAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<FilmDatabaseApp.Models.Movie> Movies { get; set; }
+
+        public System.Data.Entity.DbSet<FilmDatabaseApp.Models.Director> Directors { get; set; }
+
+        public System.Data.Entity.DbSet<FilmDatabaseApp.Models.Genre> Genres { get; set; }
+    }
+}
